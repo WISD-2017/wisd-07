b4c0f81bd4bdc9e5da1963da235117c91bb586db
Jia Yi Lai <anniuu7134@gmail.com>
2018-01-15 00:01:31 +0800

    設定組名標題

--- resources/views/aa/a1.php
+++ resources/views/aa/a1.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/11/23
- * Time: 下午 06:10
- */
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">第七組</div>
+                <div class="panel-body">
+                    @if(session('status'))
+                    <div class="alert alert-success">
+                        {{session('status')}}
+                    </div>
+                    @endif
+                    高鐵訂票系統
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
